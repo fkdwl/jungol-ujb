c0fe4ea899c026bd610848454b2a537cb3892772
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-20 12:30:07 +0900

    510번 문제 풀이

---
+++ Jungol/src/q510/Main.java
@@ -0,0 +1,24 @@
+package q510;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = -1;
+		int b = 100;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		//int num1, num2;
+		//num1 = -1;
+		//num2 = 100;
+		
+		//System.out.println(num1);
+		//System.out.println(num2);
+		
+
+	}
+
+}
