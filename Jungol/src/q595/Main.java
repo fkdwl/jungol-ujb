00f72de156cb9816b70409becb10077afb4e6049
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-21 17:08:42 +0900

    q595

---
+++ Jungol/src/q595/Main.java
@@ -0,0 +1,40 @@
+package q595;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String a = "Hong Gil Dong";
+		
+		System.out.print(a.charAt(3));
+		System.out.print(a.charAt(4));
+		System.out.print(a.charAt(5));
+		System.out.print(a.charAt(6));
+		
+		
+		//다른 풀이1
+		//String b = "Hong Gil Dong";
+		
+		//for(int i=3; i <=6; i++) {
+			//System.out.print(b.charAt(i));
+		//}
+		
+		
+		//다른 풀이2
+		//String str = "Hong Gil Dong";
+		//String ret = str.substring(3, 7);
+		//System.out.println(ret);
+		
+		
+		//다른 풀이3
+		//String a = "Hong Gil Dong";
+
+	    //String[] arry = a.split("", 0);
+
+	    //for (int i = 3; i < 7; i++) {
+	       //System.out.print(arry[i]);
+	    //}
+		
+	}
+
+}
