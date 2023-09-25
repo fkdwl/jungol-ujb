b83b33c4f0f668ad3b0f39ba0b0498cae450c249
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-25 17:17:21 +0900

    q614

---
+++ Jungol/src/q614/Student1.java
@@ -0,0 +1,38 @@
+package q614;
+
+public class Student1 {
+
+	private String school;
+	private int grade;
+
+	public Student1(int grade, String school) {
+		this.grade = grade;
+		this.school = school;
+	}
+
+	public void print() {
+		int grade1 = 6;
+		String school1 = "Jejuelementary";
+		System.out.println(grade1 + " grade in " + school1 + " School");
+		System.out.println(grade + " grade in " + school + " School");
+	}
+
+		
+		// 다른 풀이1
+		//private String school;
+		//private int grade;
+		
+		//public Student1(int grade, String school) {
+			//this.grade = grade;
+			//this.school = school;
+		//}
+		
+		//public void print() {
+			//System.out.println(grade + " grade in " + school + " School");
+		//}
+		
+		
+
+	
+
+}
