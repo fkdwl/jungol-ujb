e47e1e31de54836f31365b00610338206d18fc58
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-25 17:15:27 +0900

    q508

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,21 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "My height";
+		int b = 170;
+		String c = "My weight";
+		double d = 68.6;
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.printf("%.6f\n", d);
+		
+				
+	}
+
+}
