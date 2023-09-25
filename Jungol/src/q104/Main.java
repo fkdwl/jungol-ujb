7bc1998ff7f98132a68f40d193c9b12e4a4e7382
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-25 17:43:39 +0900

    q104

--- Jungol/src/q104/Main.java
+++ Jungol/src/q104/Main.java
@@ -9,6 +9,36 @@ public class Main {
 		System.out.println("eng " + 100);
 		System.out.println("sum " + 270);
 
+		
+		
+		//다른 풀이1
+		//int kor = 90;
+		//int mat = 80;
+		//int eng = 100;
+		
+		//System.out.println("kor " + kor);
+		//System.out.println("mat " + mat);
+		//System.out.println("eng " + eng);
+		
+		//int sum = kor + mat + eng;
+		//System.out.println("sum " + sum);
+		
+		
+		
+		//다른 풀이2
+		//int[] inp = {90, 80, 100};
+	    //int sum = 0;
+	      
+	    //for(int i=0; i < inp.length; i++) {
+	    //sum += inp[i];
+	    //}
+	      
+	    //System.out.println("kor" + " " + inp[0]);
+	    //System.out.println("mat" + " " + inp[1]);
+	    //System.out.println("eng" + " " + inp[2]);
+	    //System.out.println("sum" + " " + sum);
+		
+		
 	}
 
 }
