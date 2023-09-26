ae42daf2aea0e2c86d0f03a93d16e3688e9646f0
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-26 09:47:36 +0900

    q105

---
+++ Jungol/src/q105/Main.java
@@ -0,0 +1,39 @@
+package q105;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "Seoul";
+		String b = "10,312,545";
+		String c = "+91,375";
+		System.out.printf("%15s%15s%15s", a, b, c);
+		System.out.println();
+		
+		String d = "Pusan";
+		String e = "3,567,910";
+		String f = "+5,868";
+		System.out.printf("%15s%15s%15s", d, e, f);
+		System.out.println();
+		
+		String g = "Incheon";
+		String h = "2,758,296";
+		String i = "+64,888";
+		System.out.printf("%15s%15s%15s", g, h, i);
+		System.out.println();
+		
+		String j = "Daegu";
+		String k = "2,511,676";
+		String l = "+17,230";
+		System.out.printf("%15s%15s%15s", j, k, l);
+		System.out.println();
+		
+		String m = "Gwangju";
+		String n = "1,454,636";
+		String o = "+29,774";
+		System.out.printf("%15s%15s%15s", m, n, o);
+		
+
+	}
+
+}
