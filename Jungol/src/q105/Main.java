b7dc789ef3fb9812c9f03968999bceaa81cc832b
21-manic-010g <21-manic-010g@21-manic-010>
2023-09-26 13:35:13 +0900

    q105

--- Jungol/src/q105/Main.java
+++ Jungol/src/q105/Main.java
@@ -33,6 +33,42 @@ public class Main {
 		String o = "+29,774";
 		System.out.printf("%15s%15s%15s", m, n, o);
 		
+		
+		
+		//다른 풀이1
+		/*
+	 	ArrayList<String> pop = new ArrayList<>(
+        	Arrays.asList("Seoul", "10,312,545", "+91,375", 
+                  "Pusan", "3,567,910", "+5,868", 
+                  "Incheon", "2,758,296", "+64,888", 
+                  "Daegu", "2,511,676", "+17,230", 
+                  "Gwangju", "1,454,636", "+29,774"));
+
+      	for (int i = 0; i < pop.size(); ++i) {
+         	if (i % 3 == 0 && i != 0) {
+            	System.out.println();
+         	}
+         	System.out.printf("%15s", pop.get(i));
+      	}
+		*/
+		
+		
+		//다른 풀이2
+		/*
+		String [] A= {"Seoul", "10,312,545","+91,375", 
+            "Pusan", "3,567,910", "+5,868", 
+            "Incheon","2,758,296", "+64,888",
+            "Daegu" ,"2,511,676","+17,230",
+            "Gwangju","1,454,636" ,"+29,774"};
+      
+      	for(int i=0; i<15; i++) {
+         	System.out.printf("%15s",A[i]);
+         	if(i==2||i==5||i==8||i==11) {
+         	System.out.print("\n");
+        	}
+      	}
+		*/
+		
 
 	}
 
